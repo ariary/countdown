89dd16d0d237f0dfcd1916453ab43e592c1bfaf4
ariary <ariary9.2@hotmail.fr>
2022-10-14 17:20:25 +0200

    correct algo in combination + big int

--- src/countdown.nim
+++ src/countdown.nim
@@ -2,12 +2,13 @@ import terminal
 import std/math
 import std/algorithm
 import std/sequtils
+import bigints
 
 const PREFIX1:string =  ansiForegroundColorCode(fgBlue) & "[" &  ansiForegroundColorCode(fgCyan) & "*" &  ansiForegroundColorCode(fgBlue) & "]" & ansiForegroundColorCode(fgCyan) & " "
 const PREFIX2:string =  ansiForegroundColorCode(fgBlue) & "[" &  ansiForegroundColorCode(fgGreen) & "*" &  ansiForegroundColorCode(fgBlue) & "]" & ansiForegroundColorCode(fgGreen) & " "
 # "k parmis n"
-func binomialCoeff(n, k: int): int =
-  var res = 1
+func binomialCoeff(n, k: int64): int64 =
+  var res:int64 = 1
   var k = k
   if (k > n - k):
     k = n - k
@@ -17,8 +18,8 @@ func binomialCoeff(n, k: int): int =
   return res
 
 # "partial permutation" in english I think  = n * n-1 * ... * n-k+1
-func arrangement(n, k: int): int =
-  var res = 1
+func arrangement(n, k: int64): int64 =
+  var res:int64 = 1
   var k = k
   if (k > n - k):
     k = n - k
@@ -26,7 +27,7 @@ func arrangement(n, k: int): int =
     res = res * (n - i)
   return res
 
-proc humanPrintableInt(n:int):string=
+proc humanPrintableInt(n:BigInt):string=
     var integerStr:string =  $n
     integerStr.reverse()
     var index = 0
@@ -41,7 +42,7 @@ proc humanPrintableInt(n:int):string=
     return resStr
 
 
-proc Count(len:int,lenExtra: int,meanWordLength:int=5,maxSubstitution:int=3,maxUpper:int=1,fixedUpper:bool=false,fixedSubstitution:bool=false,maxWords:int=2,maxExtraWords:int=3,beginWithExtra:bool=false,extraFollowing:int=1,verbose:bool=true): void =
+proc Count(len:int64,lenExtra: int64,meanWordLength:int64=5,maxSubstitution:int64=3,maxUpper:int64=1,fixedUpper:bool=false,fixedSubstitution:bool=false,maxWords:int64=2,maxExtraWords:int64=3,beginWithExtra:bool=false,extraFollowing:int64=1,verbose:bool=true): void =
     if extraFollowing == 0:
         echo "--extra-following must greater than 1 (otherwise it is teh same as no extra word, in this case specify --len-extra 0"
         quit QuitFailure
@@ -57,7 +58,7 @@ proc Count(len:int,lenExtra: int,meanWordLength:int=5,maxSubstitution:int=3,maxU
     # Reconstruct wordlist
     if verbose:
         styledEcho(PREFIX1,"Reconstruct wordlist",fgDefault)
-    var nLen:int = len
+    var nLen:int64 = len
     ## Upper
     ### We consider that all the world has been change to only be composed of lower case
     ### take all the binomial coefficient from 0 to maxUpper (maybe a properties ~2^maxUpper)
@@ -65,8 +66,8 @@ proc Count(len:int,lenExtra: int,meanWordLength:int=5,maxSubstitution:int=3,maxU
         styledEcho(PREFIX2,"Add uppercase",fgDefault)
     if not fixedUpper:       
         for index in 1 .. maxUpper:
-            var oneWordVariation:int = binomialCoeff(meanWordLength,index)
-            let allVariations:int= len * oneWordVariation
+            var oneWordVariation:int64 = binomialCoeff(meanWordLength,index)
+            let allVariations:int64= len * oneWordVariation
             if verbose:
                 echo "• add \"", index, " uppercase letter(s)\" case:"
                 echo "\t➙ possible variation by words: ", oneWordVariation
@@ -87,10 +88,10 @@ proc Count(len:int,lenExtra: int,meanWordLength:int=5,maxSubstitution:int=3,maxU
     if verbose:
         styledEcho(PREFIX2,"Character substitution",fgDefault)
     if not fixedSubstitution:
-        var added:int=0
+        var added:int64=0
         for index in 1 .. maxSubstitution:
-            var oneWordVariation:int = binomialCoeff(meanWordLength,index)
-            let allVariations:int= nLen * oneWordVariation
+            var oneWordVariation:int64 = binomialCoeff(meanWordLength,index)
+            let allVariations:int64= nLen * oneWordVariation
             if verbose:
                 echo "• add \"substitute ",index," letter(s)\" case:"
                 echo "\t➙ possible variation by words: ", oneWordVariation
@@ -116,13 +117,14 @@ proc Count(len:int,lenExtra: int,meanWordLength:int=5,maxSubstitution:int=3,maxU
     # Inject the extra wordlist
     if verbose:
         styledEcho(PREFIX1,"Construct final wordlist with extra characters",fgDefault)
-    var lenFinalWordlist:int
+    # var lenFinalWordlist:int64
+    var lenFinalWordlist = 0.initBigInt
     ## Combination of word
     styledEcho(PREFIX2,"Combinations of words from list  ",styleItalic,"(no word repetition)",fgDefault)
     for index in 1 .. maxWords:
-        var wordsCombinations:int = binomialCoeff(nLen,index) # One word can't be peek twice
+        var wordsCombinations:int64 = binomialCoeff(nLen,index) # One word can't be peek twice
         ### number of insertion point for extra characters
-        var entrypoints:int
+        var entrypoints:int64
         if fixedSubstitution:
             if not beginWithExtra:
                 entrypoints = 1*extraFollowing
@@ -131,20 +133,58 @@ proc Count(len:int,lenExtra: int,meanWordLength:int=5,maxSubstitution:int=3,maxU
         else:
             entrypoints =(index+1) * extraFollowing
         
-        let extraWordsCombinations:int=lenExtra^entrypoints
-        lenFinalWordlist += extraWordsCombinations
+        var extraWordsCombinations = lenExtra.initBigInt.pow entrypoints
+        lenFinalWordlist += extraWordsCombinations * nLen.initBigInt
+        # let extraWordsCombinations:int64=lenExtra^entrypoints
+        # lenFinalWordlist += extraWordsCombinations
         if verbose:
             echo "• \"",index," word(s) from wordlist\" case:"
             echo "\t➙ possible words combinations: ", wordsCombinations
             echo "\t➙ entrypoints per combination: ", entrypoints
             echo "\t➙ possible extra words combinations: ", extraWordsCombinations
-            echo "\t➙ add combination to final wordlists length: ", extraWordsCombinations
+            echo "\t➙ add combination to final wordlists length: ", lenFinalWordlist
+        
+        ### inject extra characters
 
+
+    # var nLenExtra = lenExtra
+    # if extraFollowing > 1:
+    #     ## If extraFollowing > 1 consider adding empty set to the extra character (ease to treat all the cases), adding empty is the same has no addition
+    #     nLenExtra += 1
+    
+    # ## number of insertion point for extra characters
+    # if verbose:
+    #     styledEcho(PREFIX2,"Determine number of extra words entrypoint possibilities",fgDefault)
+    # var entrypoint:int64
+    # if fixedSubstitution:
+    #     if not beginWithExtra:
+    #         entrypoint = 1*extraFollowing
+    #     else:
+    #         entrypoint = 2*extraFollowing
+    # else:
+    #     entrypoint =(maxWords+1) * extraFollowing
+
+    # if verbose:
+    #     echo "entrypoint possibilities: ",entrypoint
+    
+    # ## Inject extra wordlist in entrypoint len ^ entrypoint
+    # if verbose:
+    #     styledEcho(PREFIX2,"Inject extra words in the entrypoints",fgDefault)
+    # var possibilities:int64 = 0
+    # for index in 1 .. maxExtraWords:
+    #     var injectionCombination = lenExtra ^ index # pow as we can have duplications
+    #     possibilities += injectionCombination
+    #     if verbose:
+    #         echo "add \"",index," extra word(s)\" case:"
+    #         echo "\tinjections pattern possible: ", injectionCombination
+
+    # possibilities *= nLen 
     if verbose:
         styledEcho fgGreen, "\n\n🧮 Possibilities: ", fgDefault, humanPrintableInt(lenFinalWordlist)
         echo "\n"
     else:
         echo lenFinalWordlist
+
 
 when isMainModule:
     import cligen;  dispatch Count, help={
